573b16d51a42933c656ee4e9c0c0488981838315
Keisuke Nakayama <baskk4.dt@gmail.com>
2019-09-06 15:27:59 +0900

    Initial Commit

---
+++ PinsOfMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PinsOfMap
+//
+//  Created by 中山慶祐 on 2019/09/06.
+//  Copyright © 2019 Keisuke Nakayama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
