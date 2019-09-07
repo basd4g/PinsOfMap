06793ff9d55b9a75d534573b74f29b49deee86bf
Keisuke Nakayama <baskk4.dt@gmail.com>
2019-09-07 14:24:35 +0900

    Add swift source file

---
+++ PinsOfMap/SearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchViewController.swift
+//  PinsOfMap
+//
+//  Created by 中山慶祐 on 2019/09/07.
+//  Copyright © 2019 Keisuke Nakayama. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
