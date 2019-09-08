3a0d5b1ae6b19046525f5afdc8eb78ffc95c1977
Keisuke Nakayama <baskk4.dt@gmail.com>
2019-09-08 21:45:30 +0900

    Display PINS to favorite table

--- PinsOfMap/FavoriteViewController.swift
+++ PinsOfMap/FavoriteViewController.swift
@@ -8,23 +8,21 @@
 
 import UIKit
 
-class FavoriteViewController: UIViewController {
+class FavoriteViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return PINS.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel!.text = PINS[indexPath.row].title
+        return cell
+    }
 
 }
